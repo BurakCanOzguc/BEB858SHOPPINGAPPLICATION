6693f88f3f76e8d13aff4a15f49cc31ac58f627b
BurakCanOzguc <148871829+BurakCanOzguc@users.noreply.github.com>
2024-02-03 01:18:41 +0300

    Drawer Menu Eklendi

--- beb858shoppingaplication/lib/main.dart
+++ beb858shoppingaplication/lib/main.dart
@@ -19,9 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),*/
-      home: GeciciUser(
-        email: 'eposta1',
-      ),
+      home: LoginPage(),
     );
   }
 }
--- beb858shoppingaplication/lib/widgets/drawer_widget.dart
+++ beb858shoppingaplication/lib/widgets/drawer_widget.dart
@@ -12,28 +12,97 @@ class _MyDrawerState extends State<MyDrawer> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Drawer(
+      backgroundColor: Color.fromARGB(255, 249, 88, 255),
       child: ListView(
         children: <Widget>[
+          //DrawerHeader********************
           DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Container(
+              //height: 200,
+              //width: double.infinity,    ****BURADA BOYUTLAR ETKİ ETMİYOR****
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.blueGrey,
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                      offset: Offset(1, 1))
+                ],
+                color: Colors.blue,
+                border: Border.all(width: 5, color: Colors.yellow),
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 93, 192, 254),
+                    Color.fromARGB(255, 249, 88, 255),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                shape: BoxShape.circle,
+              ),
               child: Align(
-            child: ListView(
-              children: <Widget>[
-                DrawerHeader(
-                  child: Align(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Icon(Icons.home,
-                            color: Colors.yellow, size: size.height * 0.1),
-                        Text("BEB858"),
-                      ],
+                child: ListView(
+                  children: <Widget>[
+                    Align(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(Icons.person,
+                              color: Colors.yellow, size: size.height * 0.1),
+                          Text("Burak Can"),
+                        ],
+                      ),
                     ),
-                  ),
-                  decoration: BoxDecoration(color: Colors.blue),
-                )
-              ],
+                    //***************DRAWERHeader
+                  ],
+                ),
+              ),
             ),
-          ))
+          ),
+          //ListTile************************
+          ListTile(
+            leading: const Icon(Icons.home, size: 40),
+            title: const Text(
+              "Page 1",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            onTap: () {},
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ListTile(
+            leading: const Icon(Icons.home, size: 40),
+            title: const Text(
+              "Page 2",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            onTap: () {},
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ListTile(
+            leading: const Icon(Icons.home, size: 40),
+            title: const Text(
+              "Page 3",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            onTap: () {},
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ListTile(
+            leading: const Icon(Icons.home, size: 40),
+            title: const Text(
+              "Page 4",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            onTap: () {},
+          ),
+          //ListTile********************
         ],
       ),
     );
